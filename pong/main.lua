066a822b418c6213009044ec701230c6ec2f5d80
John Alper <214384760+johnalper@users.noreply.github.com>
2026-06-14 23:34:17 -0400

    add retro aesthetic via push library

--- pong/main.lua
+++ pong/main.lua
@@ -1,13 +1,25 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+-- virtual window size
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
+    -- set filter to be point graphics
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
     -- set the window mode
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true,
         fullscreen = false,
     })
+
+    -- virtual layer upscales and then shrinks to fit the application window
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
 
 function love.keypressed(key)
@@ -18,8 +30,13 @@ function love.keypressed(key)
 end
 
 function love.draw()
-    -- print the message taking up full-width to center 
-    love.graphics.printf('Hello, Pong!', 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
-end
+    -- starts the magnification
+    push.start()
+    
+    -- print using virtual window width/height terms
+    love.graphics.printf('Hello, Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
 
+    -- end magnification
+    push.finish()
+end
 
